8f6db0ba9a35382c1345db7eac26dbf094a31f16
Devesh <deveshamway@gmail.com>
2024-06-02 21:41:28 +0530

    follow up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 // Footer.js
 import React from 'react';
+import { Container, Typography, Box, Paper, Grid } from '@mui/material';
 
 const Footer = () => {
     return (
-        <footer style={{ textAlign: 'center', padding: '20px', backgroundColor: '#333', color: '#fff', fontFamily: 'Arial, sans-serif', fontSize: '16px', marginTop: 'auto', width: '100%', position: 'fixed', left: 0, bottom: 0 }}>
-            Made in frustration by Devesh Yadav 😩
+        <Container style={{ padding: 0 }}>
+        <footer style={{ textAlign: 'center', padding: '15px', backgroundColor: '#333', color: '#fff', fontFamily: 'Arial, sans-serif', fontSize: '16px', marginTop: 'auto', width: '100%', position: 'fixed', left: 0, bottom: 0 }}>
+            Made in frustration by DY
         </footer>
-
+      </Container>
+      
     );
 };
 

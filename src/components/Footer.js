3591b377c9a2f82a4a04967e9dc119b38da2302d
Devesh <deveshamway@gmail.com>
2024-06-02 21:18:05 +0530

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer style={{ textAlign: 'center', padding: '20px', position: 'fixed', left: 0, bottom: 0, width: '100%', backgroundColor: '#333', color: '#fff', fontFamily: 'Arial, sans-serif', fontSize: '16px' }}>
+        <footer style={{ textAlign: 'center', padding: '20px', backgroundColor: '#333', color: '#fff', fontFamily: 'Arial, sans-serif', fontSize: '16px', position: 'fixed', left: 0, bottom: 0, width: '100%' }}>
             Made in frustration by Devesh Yadav 😩
         </footer>
     );

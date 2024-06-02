44e381043ad17443bbc86ffe0610c610097a93c6
Devesh <deveshamway@gmail.com>
2024-06-02 22:07:51 +0530

    fixing footer padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,13 @@
 // Footer.js
 import React from 'react';
-import { Container} from '@mui/material';
+// import { Container} from '@mui/material';
 
 const Footer = () => {
     return (
-        <Container style={{ padding: 0 }}>
-        <footer style={{ textAlign: 'center', padding: '15px', backgroundColor: '#333', color: '#fff', fontFamily: 'Arial, sans-serif', fontSize: '16px', marginTop: 'auto', width: '100%', position: 'fixed', left: 0, bottom: 0 }}>
-            Made in frustration by DY
-        </footer>
-      </Container>
+        <footer style={{ textAlign: 'center', padding: '10px 24px 10px 0', backgroundColor: '#333', color: '#fff', fontFamily: 'Arial, sans-serif', fontSize: '14px', marginTop: 'auto', width: '100%', position: 'fixed', left: 0, bottom: 0 }}>
+        Made in frustration by DY
+    </footer>
+    
       
     );
 };
